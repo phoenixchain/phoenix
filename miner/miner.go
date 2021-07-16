6d3802b047e1732bf602dcc9478e9203e4aea45c
Dev <dev@phoenixchain>
2021-07-16 15:04:21 +0800

    miner newWorker default init=false

--- miner/miner.go
+++ miner/miner.go
@@ -73,7 +73,7 @@ func New(eth Backend, config *Config, chainConfig *params.ChainConfig, mux *even
 		exitCh:  make(chan struct{}),
 		startCh: make(chan common.Address),
 		stopCh:  make(chan struct{}),
-		worker:  newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, true),
+		worker:  newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, false),
 	}
 	go miner.update()
 

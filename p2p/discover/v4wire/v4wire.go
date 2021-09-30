05b5fac41daf6420b6a78499a1682ea110adb555
phoenixchain@protonmail.com <phoenixchain@protonmail.com>
2021-09-30 15:44:19 +0800

    Added packet magic for v4wire.

--- p2p/discover/v4wire/v4wire.go
+++ p2p/discover/v4wire/v4wire.go
@@ -192,15 +192,21 @@ func Expired(ts uint64) bool {
 // Encoder/decoder.
 
 const (
-	macSize  = 32
-	sigSize  = crypto.SignatureLength
-	headSize = macSize + sigSize // space of packet frame data
+	magicSize = 4
+	macSize   = 32 + magicSize
+	sigSize   = crypto.SignatureLength
+	headSize  = macSize + sigSize // space of packet frame data
+)
+
+var (
+	magicv4 = []byte{0x12, 0x23, 0x34, 0x45}
 )
 
 var (
 	ErrPacketTooSmall = errors.New("too small")
 	ErrBadHash        = errors.New("bad hash")
 	ErrBadPoint       = errors.New("invalid curve point")
+	ErrPacketBadMagic = errors.New("invalid magic")
 )
 
 var headSpace = make([]byte, headSize)
@@ -210,7 +216,10 @@ func Decode(input []byte) (Packet, Pubkey, []byte, error) {
 	if len(input) < headSize+1 {
 		return nil, Pubkey{}, nil, ErrPacketTooSmall
 	}
-	hash, sig, sigdata := input[:macSize], input[macSize:headSize], input[headSize:]
+	magic, hash, sig, sigdata := input[magicSize:], input[magicSize:macSize], input[macSize:headSize], input[headSize:]
+	if !bytes.Equal(magic, magicv4) {
+		return nil, Pubkey{}, nil, ErrPacketTooSmall
+	}
 	shouldhash := crypto.Keccak256(input[macSize:])
 	if !bytes.Equal(hash, shouldhash) {
 		return nil, Pubkey{}, nil, ErrBadHash
@@ -258,7 +267,8 @@ func Encode(priv *ecdsa.PrivateKey, req Packet) (packet, hash []byte, err error)
 	copy(packet[macSize:], sig)
 	// Add the hash to the front. Note: this doesn't protect the packet in any way.
 	hash = crypto.Keccak256(packet[macSize:])
-	copy(packet, hash)
+	copy(packet[magicSize:], hash)
+	copy(packet, magicv4)
 	return packet, hash, nil
 }
 
